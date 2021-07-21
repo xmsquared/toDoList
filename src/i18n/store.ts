4fbb790c6901acdbc434a36cf361191273bb5641
xmsquared <xmdepingfang@gmail.com>
2021-07-21 17:42:36 +1000

    redux state fixing

--- src/i18n/store.ts
+++ src/i18n/store.ts
@@ -1,12 +1,28 @@
 import thunk from 'redux-thunk'
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { i18nReducer } from 'react-redux-i18n';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
 
 const store =  createStore(
     combineReducers({
       i18n: i18nReducer
     }),
-    applyMiddleware(thunk)
+    enhancer
 );
+/* eslint-enable */
 
 export default store;
